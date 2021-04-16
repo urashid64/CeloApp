0ff6b89ff4e2d6784c5a6f2aecd2a5178f73d99b
Usman Rashid <usman.rashid@gmail.com>
2021-04-16 16:52:59 -0700

    Initial Commit

---
+++ CeloApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CeloApp
+//
+//  Created by Usman Rashid on 4/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

1b705cd9acc3142f82748b27402a09e50730d1c7
Usman Rashid <usman.rashid@gmail.com>
2021-05-01 20:01:12 -0700

    Fetch balance and display it.

--- CeloApp/ViewController.swift
+++ CeloApp/ViewController.swift
@@ -10,20 +10,24 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var lblQRCode: UILabel!
+    @IBOutlet weak var lblAddress: UILabel!
     @IBOutlet weak var lblBalance: UILabel!
+
     var textObserver: NSKeyValueObservation?
 
+    var address: String!
     var balance: String!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         balance = ""
-        lblQRCode.text = balance
+        lblQRCode.text = ""
 
         textObserver = lblQRCode.observe(\.text) { [weak self] (label, observedChange) in
             self?.parseURL(text: self?.lblQRCode.text ?? "")
+            self?.fetchBalace(addr: (self?.lblQRCode.text)!)
         }
     }
     
@@ -44,9 +48,38 @@ class ViewController: UIViewController {
                 dict[item.name] = item.value!
             }
         }
+        address = dict["address"]
         let addr = String((dict["address"]?.dropFirst(2))!)
         print(addr.separate(every: 4))
-        self.lblBalance.text = addr.uppercased().separate(every: 4)
+        self.lblAddress.text = addr.uppercased().separate(every: 4)
+    }
+    
+    func fetchBalace(addr:String) -> Void {
+        let headers = ["x-api-key": "ee8102cf-dbec-4433-8bb9-37f6c98842a4"]
+
+        let url = "https://api-eu1.tatum.io/v3/celo/account/balance/\(address ?? "")"
+        let request = NSMutableURLRequest(url: NSURL(string: url)! as URL,
+                                                cachePolicy: .useProtocolCachePolicy,
+                                            timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error!)
+            }
+            else {
+                let returnData = String(data: data!, encoding: .utf8)
+                print (returnData!)
+                self.balance = returnData
+                DispatchQueue.main.async {
+                    self.lblBalance.text = returnData
+                }
+             }
+        })
+
+        dataTask.resume()
     }
 }
 

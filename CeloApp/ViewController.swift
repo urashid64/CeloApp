5fba2714428f0b49958186fbb725672761aba8b3
Usman Rashid <usman.rashid@gmail.com>
2021-05-01 23:06:17 -0700

    Decode JSON data.  MVP - 0.1

--- CeloApp/ViewController.swift
+++ CeloApp/ViewController.swift
@@ -18,12 +18,24 @@ class ViewController: UIViewController {
     var address: String!
     var balance: String!
 
+    struct celoAccount: Decodable {
+        enum Category: String, Decodable {
+            case swift, combine, debugging, xcode
+        }
+
+        let celo: String
+        let cUsd: String
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         balance = ""
         lblQRCode.text = ""
+        lblAddress.text = ""
+        lblBalance.text = ""
 
         textObserver = lblQRCode.observe(\.text) { [weak self] (label, observedChange) in
             self?.parseURL(text: self?.lblQRCode.text ?? "")
@@ -31,12 +43,14 @@ class ViewController: UIViewController {
         }
     }
     
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showScanner" {
             let scannerVC = segue.destination as! ScannerViewController
             scannerVC.callback = { text in self.lblQRCode.text = text }
         }
     }
+    
     
     func parseURL(text:String) -> Void {
         guard let url = URL(string:text) else { return }
@@ -50,14 +64,16 @@ class ViewController: UIViewController {
         }
         address = dict["address"]
         let addr = String((dict["address"]?.dropFirst(2))!)
-        print(addr.separate(every: 4))
+        
         self.lblAddress.text = addr.uppercased().separate(every: 4)
+        self.lblBalance.text = "Fetching..."
     }
+    
     
     func fetchBalace(addr:String) -> Void {
         let headers = ["x-api-key": "ee8102cf-dbec-4433-8bb9-37f6c98842a4"]
-
         let url = "https://api-eu1.tatum.io/v3/celo/account/balance/\(address ?? "")"
+
         let request = NSMutableURLRequest(url: NSURL(string: url)! as URL,
                                                 cachePolicy: .useProtocolCachePolicy,
                                             timeoutInterval: 10.0)
@@ -71,17 +87,28 @@ class ViewController: UIViewController {
             }
             else {
                 let returnData = String(data: data!, encoding: .utf8)
-                print (returnData!)
                 self.balance = returnData
+                print (returnData!)
+
+                let acct: celoAccount = try! JSONDecoder().decode(celoAccount.self, from: data!)
+                print ("Celo: \(acct.celo)")
+                print ("cUsd: \(acct.cUsd)")
+                
                 DispatchQueue.main.async {
-                    self.lblBalance.text = returnData
+                    self.formatBalance(account: acct)
                 }
              }
         })
 
         dataTask.resume()
     }
+    
+    
+    func formatBalance(account: celoAccount) -> Void {
+        lblBalance.text = String("CELO: \(account.celo)\ncUSD: \(account.cUsd)")
+    }
 }
+
 
 extension String {
     func separate(every stride: Int = 4, with separator: Character = " ") -> String {
